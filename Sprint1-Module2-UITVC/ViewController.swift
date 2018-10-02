f6fa8f0792a91eaf214936d8763b88cbe91bfb62
hendoware <hendoware@gmail.com>
2018-10-02 13:17:22 -0400

    Initial Commit

---
+++ Sprint1-Module2-UITVC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sprint1-Module2-UITVC
+//
+//  Created by Sean Hendrix on 10/2/18.
+//  Copyright © 2018 Sean Hendrix. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
